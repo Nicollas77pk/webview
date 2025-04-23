ea7cafd4172ccc80a1297935edf102a04abf610b
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-23 17:34:09 -0300

    Create index.js

---
+++ webview-app/index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
